c7dcdbbdcef3f6bd2c094ad99e8239baaa068c4b
Simon K <30438951+simshard@users.noreply.github.com>
2019-08-16 13:28:32 +0100

    tweaks and stuff

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Sykes Cottages Search demo
  * @author Name <simon.kember@blueyonder.co.uk>
- * 
+ *
  */
 $MYSQL_HOST='localhost';
 $MYSQL_USER='root';
@@ -15,12 +15,23 @@ if (mysqli_connect_errno()) {
     echo "<strong>Failed to connect to MySQL</strong>: " . mysqli_connect_error();
     exit;
 }
-if (isset($_GET['page']) & !empty($_GET['page'])) {
-    $curpage = $_GET['page'];
-} else {
-    $curpage = 1;
-}
+?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <title>Sykes Cottages</title>
+</head>
+
+<body>
+  <div class="container">
+<?php
 
 if (isset($_POST['searchform'])&& !empty($_POST)) {
     
@@ -34,16 +45,19 @@ if (isset($_POST['searchform'])&& !empty($_POST)) {
     $beach=isset($_POST['beach']) ? $_POST['beach']:0;
     $datepicker=isset($_POST['datepicker'])? $_POST['datepicker']:null;
 
+    $offset=0;
     $pagelimit = 2;
-    $totalrowsquery="SELECT count(*) AS totalrows FROM `locations` AS loc
-                     LEFT JOIN `properties` AS prop ON loc.__pk=prop._fk_location";
-    $totalpages = mysqli_query($mysqli, $totalrowsquery);
-    $totalrows = mysqli_num_rows($totalpages);
-    $start = ($curpage * $pagelimit) - $pagelimit;
-    $endpage = ceil($totalrows/$pagelimit);
-    $startpage = 1;
-    $nextpage = $curpage + 1;
-    $previouspage = $curpage - 1;
+    if (isset($_GET['page']) & !empty($_GET['page'])) {
+        $curpage = $_GET['page'];
+        if ($curpage>1) {
+            $offset = ($curpage - 1) * $pagelimit;
+        }
+    } else {
+        $curpage = 1;
+    }
+    
+
+    
 
 
     $query="SELECT 
@@ -68,12 +82,27 @@ if (isset($_POST['searchform'])&& !empty($_POST)) {
     if ($beds) {
         $query.=" AND `beds`=$beds";
     }
-     if ($datepicker) {
+    if ($datepicker) {
         $query.=" AND  $datepicker NOT BETWEEN book.start_date AND book.end_date ";
     }
     
-    //$query.="LIMIT $start, $pagelimit";
+   // $query.="LIMIT $offset, $pagelimit";
     
+    $totalrowsquery="SELECT *
+                     FROM `locations` AS loc
+                     LEFT JOIN `properties` AS prop
+                     ON loc.__pk=prop._fk_location";
+    $totalres = mysqli_query($mysqli, $totalrowsquery);
+      $totalrows = mysqli_num_rows($totalres);
+      
+    
+      $start = ($curpage * $pagelimit) - $pagelimit;
+      $endpage = ceil($totalrows/$pagelimit);
+      $startpage = 1;
+      $nextpage = $curpage + 1;
+      $previouspage = $curpage - 1;
+     
+   // echo 'totalrows:'.$totalrows.' endpage:'.$endpage.' curpage:'.$curpage.' start:'.$start.' offset:'.$offset ;
     $results='';
 
     $result =$mysqli->query($query) or die(mysqli_error($mysqli));
@@ -95,31 +124,18 @@ if (isset($_POST['searchform'])&& !empty($_POST)) {
         $results.='</li>';
     }
       
-    echo('<h3>Search Results</h3><ul>'.$results.'</ul>');
+    echo('<h3>Search Results</h3><ul>'.$results.'</ul><p><a href="index.php">Search again</a></p>');
+    
         
     if (empty($results)) {
-        echo('<p>No results</p>');
+        echo('<p>No results  <a href="index.php">Search again</a></p>');
     }
-    echo('<small>Query:'.$query.'</small>');
+    echo('<small class="text-success"><em>Query:</em>'.$query.'</small>');
 
     $mysqli->close();
 } else {
     ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <title>Sykes Cottages</title>
-</head>
-
-<body>
-  <div class="container">
     <h1>Search for your Holiday Rental</h1>
 
     <form name="search" action="" method="POST">
@@ -179,28 +195,28 @@ if (isset($_POST['searchform'])&& !empty($_POST)) {
 function Sanitizes_text($value)
 {
     if (get_magic_quotes_gpc()) {
-         $value = stripslashes($value);
+        $value = stripslashes($value);
     }
-     $value=strip_tags($value);
+    $value=strip_tags($value);
     $value=filter_var(
         $value,
         FILTER_SANITIZE_STRING,
         FILTER_FLAG_STRIP_HIGH|FILTER_FLAG_STRIP_LOW
     );
-     $value=htmlspecialchars($value, ENT_QUOTES);
-     $crlf=array('&#13;','&#10;');
-     $value= str_replace($crlf, '', $value);
-     return trim($value);
+    $value=htmlspecialchars($value, ENT_QUOTES);
+    $crlf=array('&#13;','&#10;');
+    $value= str_replace($crlf, '', $value);
+    return trim($value);
 }
 
  
 function Sanitizes_numeric($value)
 {
     if (get_magic_quotes_gpc()) {
-         $value = stripslashes($value);
+        $value = stripslashes($value);
     }
-     $value=filter_var($value, FILTER_SANITIZE_NUMBER_INT);//integers only
-     return trim($value);
+    $value=filter_var($value, FILTER_SANITIZE_NUMBER_INT);//integers only
+    return trim($value);
 }
 
     /*
